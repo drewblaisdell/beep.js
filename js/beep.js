80bf28efe6f5dbc4f2e7caf41bb26ac813a16edd
Drew Blaisdell <drew.blaisdell@gmail.com>
2013-10-25 12:56:44 -0700

    time parameter

--- js/beep.js
+++ js/beep.js
@@ -2,17 +2,19 @@
 	window.AudioContext = window.AudioContext || window.webkitAudioContext;
 	this.that = this;
 
-	var beep = beep || function(){
-	    beep.oscillator.connect(beep.context.destination);
-	    beep.oscillator.start(0);
+	var beep = beep || function(time){
+		var time = time || 500;
+		beep.oscillator.connect(beep.context.destination);
+		beep.oscillator.start(0);
 
-	    setTimeout(function(){
-	    	beep.oscillator.disconnect();
-	    }, 300)
+		setTimeout(function(){
+			beep.oscillator.disconnect();
+		}, time)
 	};
 
-    beep.context = beep.context || new AudioContext();
+	beep.context = beep.context || new AudioContext();
 	beep.oscillator = beep.oscillator || beep.context.createOscillator();
+	beep.oscillator.type = 1;
 
 	this.beep = beep;
 
